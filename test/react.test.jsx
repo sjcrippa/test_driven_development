923045cfdbbf630bf85ff9eb75a431203964841c
Santiago Crippa <santicrippa@gmail.com>
2023-07-28 12:30:34 -0300

    addeing react properly

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,8 +1,17 @@
-import { render } from '@testing-library/react'
+import React from 'react'
+import { render, screen } from '@testing-library/react'
 import { describe, it } from 'vitest'
 
+const Calculator = () => { }
+
 describe('Calculator', () => {
-  it('should render', () => {
+  it('should render component', () => {
     render(<Calculator />)
+  })
+
+  it('should render title correctly', () => {
+    render(<Calculator />)
+
+    screen.getByText('Calculator')
   })
 })
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   pluggins: [react()],
-  test: { // config de vitest
+  test: {
     environment: 'happy-dom' // happy-dom es una herramienta que emula un navegador parcheando todas las apis de la web.
   }
 })
